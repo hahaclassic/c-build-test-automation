6758f0576308c6fe3201e7ed368c69fa02799364
Nikita Belyaev <belyaev.frog01@gmail.com>
2023-03-06 20:01:57 +0300

    RELEASE

--- num_comparator.sh
+++ num_comparator.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "comparing!"
 if [ -n "$3" ]; then
   flag_verbose="-v"
 else
@@ -12,9 +11,10 @@ print_debugging()
   if [ -n "$flag_verbose" ] && [ -n "$2" ]; then
     printf "%s\n" "$1"
     cat "$2"
-  elif [ -n "$flag_verbose" ] && [ -z "$2" ]; then
+  elif [ -n "$flag_verbose" ] && [ -z "$2" ] && [ "$1" != "\n" ]; then
     printf "%s\n" "$1"
-
+  elif [ -n "$flag_verbose" ] && [ "$1" == "\n" ]; then
+    printf "\n"
   fi
   #printf "\n"
 }
@@ -25,8 +25,8 @@ regex="^[+-]?[0-9]+[.]?[0-9]+([e][+-]?[0-9]+)?$"
 file_1=$1
 file_2=$2
 
-cat "$1"
-cat "$2"
+print_debugging "Expected No.1: " "$file_1"
+print_debugging "Actual No.2: " "$file_2"
 
 touch /tmp/buffer_1
 touch /tmp/buffer_2
@@ -35,7 +35,6 @@ while IFS= read -r line
 do
   for word in $line; do
     if [[ $word =~ $regex ]]; then
-      echo "$word"
       echo "$word" >> /tmp/buffer_1
     fi
   done
@@ -50,12 +49,10 @@ do
   done
 done < "$file_2"
 
-print_debugging "Buffer No.1: " "/tmp/buffer_1"
 
-echo "buffs:"
-cat /tmp/buffer_1
-echo "@@@"
-cat /tmp/buffer_2
+print_debugging "Buffer No.1: " "/tmp/buffer_1"
+print_debugging "Buffer No.2: " "/tmp/buffer_2"
+print_debugging "\n"
 
 if cmp -s /tmp/buffer_1 /tmp/buffer_2; then
   rm /tmp/buffer_1 /tmp/buffer_2
