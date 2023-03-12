3876fe9353b18c0eea9ae6b33a76ad6762a72922
Nikita Belyaev <belyaev.frog01@gmail.com>
2023-03-12 20:51:14 +0300

    cbuild checks for func_tests/ existing in order to prevent data rewriting

--- .c-builder.sh
+++ .c-builder.sh
@@ -8,6 +8,12 @@ destination="$1"
 
 cd ~/
 cp Programming/clabs/testing_scripts/*sh "$destination"
-cp -r Programming/clabs/testing_scripts/func_tests "$destination"
-}
+
+if [[ -d "Programming/clabs/testing_scripts/func_tests/" ]]; then
+  cp -r Programming/clabs/testing_scripts/func_tests/scripts "$destination"
+else
+  cp -r Programming/clabs/testing_scripts/func_tests/ "$destination"
+fi
+
+
 
